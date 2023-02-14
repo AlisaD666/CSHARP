898aaf779a892b56cf2ce45cb5b48d1fbeed25e8
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-14 14:46:42 +0300

    zadacha

---
+++ lession_TWO/seminar2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число первое ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число второе ");
+int y = int.Parse(Console.ReadLine());
+
+if (x % y == 0) Console.WriteLine("Второе число кратно первому");
+else
+{
+    Console.Write("Не кратно, остаток = ");
+    Console.WriteLine(x % y);
+}
