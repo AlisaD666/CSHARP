12c4a92305a33cd9e58c8275146b8b5bbfe24405
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-14 14:36:06 +0300

    задачи

---
+++ lession_TWO/seminar2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine(x);
+if (x % 7 == 0)
+{
+    x = x / 7;
+    if (x % 23 == 0)
+    {
+        Console.WriteLine("Число кратно 7 и 23");
+    }
+    else
+    {
+        Console.WriteLine("Число кратно 7");
+    }
+}
+else
+{
+    if (x % 23 == 0)
+    {
+        Console.WriteLine("Число кратно 23");
+    }
+    else
+    {
+        Console.WriteLine("Число не кратно 7 и 23");
+    }
+}
