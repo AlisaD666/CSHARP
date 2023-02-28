76fdef020d4e91f3c368e87ad2760df7c97c4c09
Alisa Dhelomanova <89995351029@mail.ru>
2023-02-28 13:37:55 +0300

    Задачи с лекции

---
+++ lession_TWO/seminar2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два целых числа");
+Console.WriteLine("Первое число: ");
+int number_one = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int number_two = Convert.ToInt32(Console.ReadLine());
+
+if (number_one == number_two*number_two) {
+    Console.WriteLine("Yes");
+} else {
+    Console.WriteLine("No");
+}
